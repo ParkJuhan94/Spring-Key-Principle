08dece0ecf6a09ced8393227d07989f658854342
ParkJuHan94 <1004sunye@naver.com>
2023-06-11 13:09:25 +0900

    0611

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -8,7 +8,14 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
+
         MemberService memberService = new MemberServiceImpl();
+
         Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
     }
 }
