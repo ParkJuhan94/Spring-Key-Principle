cfe98415b41d7247ac24925775e943b2a482fa5d
ParkJuHan94 <1004sunye@naver.com>
2023-07-07 23:07:15 +0900

    0707

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -11,7 +12,13 @@ import java.nio.file.DirectoryStream;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // discountPolicy 의 구현체를 지운다 (dip, ocp 위반) -> 인터페이스에만 의존하도록 만들어준다.
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // DiscountPolicy 의 구현 객체를 누군가가 대신 생성하고 주입해주어야한다.
+    private  DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
