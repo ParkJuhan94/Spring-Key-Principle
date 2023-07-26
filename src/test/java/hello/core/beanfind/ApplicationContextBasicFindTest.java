bdec73372d3bbf2ac0998b8061f19e19b36114dd
ParkJuHan94 <1004sunye@naver.com>
2023-07-26 19:24:26 +0900

    0726

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,54 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.MemberApp;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+// Assertions 에 커서를 두고 (옵션 + 엔터)
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName()
+    {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType()
+    {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    // 역할에 의존하지 않고 구현에 의존했기 때문에 좋은 테스트법은 아니야. 그러나 필요할 때가 있을거야.
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2()
+    {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX()
+    {
+//        ac.getBean("xxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
+}
--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -7,7 +7,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;    // Assertions 에 커서를 두고 옵션+엔터
 
 class RateDiscountPolicyTest {
 
