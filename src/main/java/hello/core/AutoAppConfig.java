8c775499c7439eda2281cd15ba4dc78380d393c5
ParkJuHan94 <1004sunye@naver.com>
2023-08-30 18:21:49 +0900

    0830

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -13,3 +13,13 @@ import static org.springframework.context.annotation.ComponentScan.*;
 public class AutoAppConfig {
 
 }
+
+/*
+컴포넌트 스캔의 용도 뿐만 아니라 다음 애노테이션이 있으면 스프링은 부가 기능을 수행한다.
+@Controller : 스프링 MVC 컨트롤러로 인식
+@Repository : 스프링 데이터 접근 계층으로 인식하고, 데이터 계층의 예외를 스프링 예외로 변환해준다.
+@Configuration : 앞서 보았듯이 스프링 설정 정보로 인식하고, 스프링 빈이 싱글톤을 유지하도록 추가
+처리를 한다.
+@Service : 사실 @Service 는 특별한 처리를 하지 않는다. 대신 개발자들이 핵심 비즈니스 로직이 여기에
+있겠구나 라고 비즈니스 계층을 인식하는데 도움이 된다.
+ */
